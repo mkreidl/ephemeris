948115fcf4103f3e9ad319f73f9b6bc68b05d2aa
Martin Kreidl <martin.kreidl.privat@gmail.com>
2018-04-18 21:54:19 +0200

    Code formatted; java.time replaced by threetenbp

--- src/main/java/com/mkreidl/ephemeris/dynamics/Pluto.java
+++ src/main/java/com/mkreidl/ephemeris/dynamics/Pluto.java
@@ -6,7 +6,7 @@ import com.mkreidl.ephemeris.geometry.Spherical;
 
 public class Pluto extends OrbitalModel<Cartesian>
 {
-  private final Spherical posSpherical = new Spherical();
+    private final Spherical posSpherical = new Spherical();
 
     @Override
     public void compute( Time time, Cartesian position, Cartesian velocity )
@@ -17,7 +17,7 @@ public class Pluto extends OrbitalModel<Cartesian>
         final double s1 = Math.toRadians( 0.033459652 );
         final double p0 = Math.toRadians( 238.95 );
         final double p1 = Math.toRadians( 0.003968789 );
-        
+
         final double s = s0 + s1 * d;
         final double p = p0 + p1 * d;
 
@@ -49,37 +49,37 @@ public class Pluto extends OrbitalModel<Cartesian>
         if ( velocity != null )
         {
             final double lp = Math.toRadians( 0.00400703 + p1 * (
-                    - 19.799 * Math.cos( p ) - 19.848 * Math.sin( p )
-                    + 0.897 * 2 * Math.cos( 2 * p ) + 4.956 * 2 * Math.sin( 2 * p )
-                    + 0.610 * 3 * Math.cos( 3 * p ) - 1.211 * 3 * Math.sin( 3 * p )
-                    - 0.341 * 4 * Math.cos( 4 * p ) + 0.190 * 4 * Math.sin( 4 * p )
-                    + 0.128 * 5 * Math.cos( 5 * p ) + 0.034 * 5 * Math.sin( 5 * p )
-                    - 0.038 * 6 * Math.cos( 6 * p ) - 0.031 * 6 * Math.sin( 6 * p ) )
+                    -19.799 * Math.cos( p ) - 19.848 * Math.sin( p )
+                            + 0.897 * 2 * Math.cos( 2 * p ) + 4.956 * 2 * Math.sin( 2 * p )
+                            + 0.610 * 3 * Math.cos( 3 * p ) - 1.211 * 3 * Math.sin( 3 * p )
+                            - 0.341 * 4 * Math.cos( 4 * p ) + 0.190 * 4 * Math.sin( 4 * p )
+                            + 0.128 * 5 * Math.cos( 5 * p ) + 0.034 * 5 * Math.sin( 5 * p )
+                            - 0.038 * 6 * Math.cos( 6 * p ) - 0.031 * 6 * Math.sin( 6 * p ) )
                     + ( s1 - p1 ) * ( 0.020 * Math.cos( s - p ) + 0.010 * Math.sin( s - p ) ) );
-    
+
             final double bp = Math.toRadians( p1 * (
-                    - 5.453 * Math.cos( p ) + 14.975 * Math.sin( p )
-                    + 3.527 * 2 * Math.cos( 2 * p ) - 1.673 * 2 * Math.sin( 2 * p )
-                    - 1.051 * 3 * Math.cos( 3 * p ) - 0.328 * 3 * Math.sin( 3 * p )
-                    + 0.179 * 4 * Math.cos( 4 * p ) + 0.292 * 4 * Math.sin( 4 * p )
-                    + 0.019 * 5 * Math.cos( 5 * p ) - 0.100 * 5 * Math.sin( 5 * p )
-                    - 0.031 * 6 * Math.cos( 6 * p ) + 0.026 * 6 * Math.sin( 6 * p ) )
+                    -5.453 * Math.cos( p ) + 14.975 * Math.sin( p )
+                            + 3.527 * 2 * Math.cos( 2 * p ) - 1.673 * 2 * Math.sin( 2 * p )
+                            - 1.051 * 3 * Math.cos( 3 * p ) - 0.328 * 3 * Math.sin( 3 * p )
+                            + 0.179 * 4 * Math.cos( 4 * p ) + 0.292 * 4 * Math.sin( 4 * p )
+                            + 0.019 * 5 * Math.cos( 5 * p ) - 0.100 * 5 * Math.sin( 5 * p )
+                            - 0.031 * 6 * Math.cos( 6 * p ) + 0.026 * 6 * Math.sin( 6 * p ) )
                     - ( s1 - p1 ) * 0.011 * Math.sin( s - p ) );
-    
+
             final double rp = p1 * (
-                    + 6.68 * Math.cos( p ) - 6.90 * Math.sin( p )
-                    - 1.18 * 2 * Math.cos( 2 * p ) + 0.03 * 2 * Math.sin( 2 * p )
-                    + 0.15 * 3 * Math.cos( 3 * p ) + 0.14 * 3 * Math.sin( 3 * p ) );
-    
+                    +6.68 * Math.cos( p ) - 6.90 * Math.sin( p )
+                            - 1.18 * 2 * Math.cos( 2 * p ) + 0.03 * 2 * Math.sin( 2 * p )
+                            + 0.15 * 3 * Math.cos( 3 * p ) + 0.14 * 3 * Math.sin( 3 * p ) );
+
             final double r = posSpherical.dst;
             final double sl = Math.sin( posSpherical.lon );
             final double sb = Math.sin( posSpherical.lat );
             final double cl = Math.cos( posSpherical.lon );
             final double cb = Math.cos( posSpherical.lat );
-    
-            velocity.x = ( rp * cl * cb - r * (lp * sl * cb + bp * cl * sb) ) / Time.SECONDS_PER_DAY;
-            velocity.y = ( rp * sl * cb + r * (lp * cl * cb - bp * sl * sb) ) / Time.SECONDS_PER_DAY;
-            velocity.z = ( rp * sb      + r * bp * cb ) / Time.SECONDS_PER_DAY;
+
+            velocity.x = ( rp * cl * cb - r * ( lp * sl * cb + bp * cl * sb ) ) / Time.SECONDS_PER_DAY;
+            velocity.y = ( rp * sl * cb + r * ( lp * cl * cb - bp * sl * sb ) ) / Time.SECONDS_PER_DAY;
+            velocity.z = ( rp * sb + r * bp * cb ) / Time.SECONDS_PER_DAY;
         }
     }
 
--- src/test/java/com/mkreidl/ephemeris/sky/PositionsMotionsTest.java
+++ src/test/java/com/mkreidl/ephemeris/sky/PositionsMotionsTest.java
@@ -1,13 +1,5 @@
 package com.mkreidl.ephemeris.sky;
 
-import static com.mkreidl.ephemeris.sky.SolarSystem.Body.EARTH;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import java.util.Arrays;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import com.mkreidl.ephemeris.Distance;
 import com.mkreidl.ephemeris.TestUtil;
 import com.mkreidl.ephemeris.TestUtil.EphemerisData;
@@ -18,66 +10,80 @@ import com.mkreidl.ephemeris.sky.SolarSystem.Body;
 import com.mkreidl.ephemeris.sky.coordinates.Ecliptical;
 import com.mkreidl.ephemeris.sky.coordinates.Equatorial;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+
+import static com.mkreidl.ephemeris.sky.SolarSystem.Body.EARTH;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Parses txt test data produced by http://ephemeris.com/ephemeris.php
  */
-@RunWith(Parameterized.class)
-public class PositionsMotionsTest {
+@RunWith( Parameterized.class )
+public class PositionsMotionsTest
+{
+    private static final double TOL_PLANET = 0.58 * Angle.MIN;
+    private static final double TOL_MOON = 4 * Angle.MIN;
 
-  private static final double TOL_PLANET = 0.58 * Angle.MIN;
-  private static final double TOL_MOON = 4 * Angle.MIN;
+    private final SolarSystem solarSystem = new SolarSystem();
+    private final Time time;
+    private final SolarSystem.Body body;
+    private final EphemerisData expected;
+    private final Ecliptical.Sphe ecliptical = new Ecliptical.Sphe();
+    private final Equatorial.Sphe equatorial = new Equatorial.Sphe();
 
-  private final SolarSystem solarSystem = new SolarSystem();
-  private final Time time;
-  private final SolarSystem.Body body;
-  private final EphemerisData expected;
-  private final Ecliptical.Sphe ecliptical = new Ecliptical.Sphe();
-  private final Equatorial.Sphe equatorial = new Equatorial.Sphe();
+    @Parameters( name = "{0}" )
+    public static Iterable<Object[]> data()
+    {
+        return TestUtil.solarSystemData( Arrays.asList( SolarSystem.Body.values() ) );
+    }
 
-  @Parameters(name = "{0}")
-  public static Iterable<Object[]> data() {
-    return TestUtil.solarSystemData(Arrays.asList(SolarSystem.Body.values()));
-  }
+    public PositionsMotionsTest( String testname, SolarSystem.Body body, Time time, EphemerisData expected )
+    {
+        this.body = body;
+        this.time = time;
+        this.expected = expected;
+    }
 
-  public PositionsMotionsTest(String testname, SolarSystem.Body body, Time time, EphemerisData expected) {
-    this.body = body;
-    this.time = time;
-    this.expected = expected;
-  }
+    @Test
+    public void testGeocentricCoordinates()
+    {
+        solarSystem.compute( time, body );
+        solarSystem.compute( time, EARTH );
 
-  @Test
-  public void testGeocentricCoordinates() {
-    solarSystem.compute(time, body);
-    solarSystem.compute(time, EARTH);
+        final Position actual = solarSystem.getEphemerides( body, new Position() );
+        actual.setTimeLocation( time, new Spherical() );
+        actual.get( ecliptical, Position.CoordinatesCenter.GEOCENTRIC );
+        actual.get( equatorial, Position.CoordinatesCenter.GEOCENTRIC );
 
-    final Position actual = solarSystem.getEphemerides(body, new Position());
-    actual.setTimeLocation(time, new Spherical());
-    actual.get(ecliptical, Position.CoordinatesCenter.GEOCENTRIC);
-    actual.get(equatorial, Position.CoordinatesCenter.GEOCENTRIC);
+        final Angle longitude = ecliptical.getLongitude( new Angle.Sexagesimal( Angle.Unit.DEGREES ) );
+        final Angle latitude = ecliptical.getLatitude( new Angle.Sexagesimal( Angle.Unit.DEGREES ) );
+        final Angle rightAscension = equatorial.getRightAscension( new Angle.Sexagesimal( Angle.Unit.HOURS ) );
+        final Angle declination = equatorial.getDeclination( new Angle.Sexagesimal( Angle.Unit.DEGREES ) );
 
-    final Angle longitude = ecliptical.getLongitude(new Angle.Sexagesimal(Angle.Unit.DEGREES));
-    final Angle latitude = ecliptical.getLatitude(new Angle.Sexagesimal(Angle.Unit.DEGREES));
-    final Angle rightAscension = equatorial.getRightAscension(new Angle.Sexagesimal(Angle.Unit.HOURS));
-    final Angle declination = equatorial.getDeclination(new Angle.Sexagesimal(Angle.Unit.DEGREES));
+        System.out.println( "-------------------------------------" );
+        System.out.println( time + " -- " + body );
+        System.out.println( time.julianDayNumber() );
+        System.out.println( "-------------------------------------" );
 
-    System.out.println("-------------------------------------");
-    System.out.println(time + " -- " + body);
-    System.out.println(time.julianDayNumber());
-    System.out.println("-------------------------------------");
+        System.out.println( "Distance: " + equatorial.distance( Distance.AU ) );
+        System.out.println( String.format( "%s  | ecliptical longitude |  %s", expected.longitude, longitude ) );
+        System.out.println( String.format( "%s  | ecliptical latitude  |  %s", expected.latitude, latitude ) );
+        System.out.println( String.format( "%s  | equatorial longitude |  %s", expected.rightAscension, rightAscension ) );
+        System.out.println( String.format( "%s  | equatorial latitude  |  %s", expected.declination, declination ) );
+        System.out.println( "expected: " + ( expected.retrograde ? "R" : "-" ) + "  |  actual: " + ( actual.isRetrograde() ? "R" : "-" ) );
 
-    System.out.println("Distance: " + equatorial.distance(Distance.AU));
-    System.out.println(String.format("%s  | ecliptical longitude |  %s", expected.longitude, longitude));
-    System.out.println(String.format("%s  | ecliptical latitude  |  %s", expected.latitude, latitude));
-    System.out.println(String.format("%s  | equatorial longitude |  %s", expected.rightAscension, rightAscension));
-    System.out.println(String.format("%s  | equatorial latitude  |  %s", expected.declination, declination));
-    System.out.println("expected: " + (expected.retrograde ? "R" : "-") + "  |  actual: " + (actual.isRetrograde() ? "R" : "-"));
+        final double tol = body == Body.MOON ? TOL_MOON : TOL_PLANET;
 
-    final double tol = body == Body.MOON ? TOL_MOON : TOL_PLANET;
-    
-    assertEquals(expected.longitude.getRadians(), longitude.getRadians(), tol);
-    assertEquals(expected.latitude.getRadians(), latitude.getRadians(), tol);
-    assertEquals(expected.rightAscension.getRadians(), rightAscension.getRadians(), tol);
-    assertEquals(expected.declination.getRadians(), declination.getRadians(), tol);
-    assertTrue(expected.retrograde == actual.isRetrograde());
-  }
+        assertEquals( expected.longitude.getRadians(), longitude.getRadians(), tol );
+        assertEquals( expected.latitude.getRadians(), latitude.getRadians(), tol );
+        assertEquals( expected.rightAscension.getRadians(), rightAscension.getRadians(), tol );
+        assertEquals( expected.declination.getRadians(), declination.getRadians(), tol );
+        assertTrue( expected.retrograde == actual.isRetrograde() );
+    }
 }
