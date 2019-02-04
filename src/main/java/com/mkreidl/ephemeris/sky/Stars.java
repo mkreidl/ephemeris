4f627960caa3207a658fc163481c9f426a3b8c67
Martin Kreidl <martin.kreidl@vsa.de>
2019-02-04 18:45:03 +0100

    Continued star test

--- src/main/java/com/mkreidl/ephemeris/sky/Stars.java
+++ src/main/java/com/mkreidl/ephemeris/sky/Stars.java
@@ -10,24 +10,21 @@ import com.mkreidl.ephemeris.solarsystem.Ecliptic;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
 
-final public class Stars
-{
+final public class Stars {
     static final double[] POS_J2000 = new double[StarsCatalog.SIZE * 3];
     static final double[] VEL_J2000 = new double[StarsCatalog.SIZE * 3];
 
-    static
-    {
-        final Matrix3x3 equ2ecl = new Matrix3x3(new Ecliptic(Time.J2000).getTrafoMeanEqu2Ecl() );
+    static {
+        final Matrix3x3 equ2ecl = new Matrix3x3(new Ecliptic(Time.J2000).getTrafoMeanEqu2Ecl());
         final Matrix3x3 jacobian = new Matrix3x3();
         final Cartesian tmp = new Cartesian();
         final Equatorial.Cart velEquatorial = new Equatorial.Cart();
         final Equatorial.Cart posEquatorial = new Equatorial.Cart();
         final Equatorial.Sphe posJ2000 = new Equatorial.Sphe();
 
-        for ( int i = 0; i < StarsCatalog.SIZE; ++i )
-        {
-            final double ra = StarsCatalog.getRAscJ2000( i );
-            final double decl = StarsCatalog.getDeclJ2000( i );
+        for (int i = 0; i < StarsCatalog.SIZE; ++i) {
+            final double ra = StarsCatalog.getRAscJ2000(i);
+            final double decl = StarsCatalog.getDeclJ2000(i);
             final double sinRa = sin(ra);
             final double cosRa = cos(ra);
             final double sinDecl = sin(decl);
@@ -37,13 +34,13 @@ final public class Stars
                     cosRa * cosDecl, -sinRa * sinDecl, 0,
                     0, cosDecl, 0
             );
-            tmp.set( StarsCatalog.getVRAscJ2000( i ), StarsCatalog.getVDeclJ2000( i ), 0 );
-            jacobian.applyTo( tmp, velEquatorial );
-            final Cartesian vel = equ2ecl.applyTo( velEquatorial, new Ecliptical.Cart() );
+            tmp.set(StarsCatalog.getVRAscJ2000(i), StarsCatalog.getVDeclJ2000(i), 0);
+            jacobian.applyTo(tmp, velEquatorial);
+            final Cartesian vel = equ2ecl.applyTo(velEquatorial, new Ecliptical.Cart());
 
-            posJ2000.set( 1.0, StarsCatalog.getRAscJ2000( i ), StarsCatalog.getDeclJ2000( i ) );
-            posJ2000.transform( posEquatorial );
-            final Cartesian pos = equ2ecl.applyTo( posEquatorial, new Ecliptical.Cart() );
+            posJ2000.set(1.0, StarsCatalog.getRAscJ2000(i), StarsCatalog.getDeclJ2000(i));
+            posJ2000.transform(posEquatorial);
+            final Cartesian pos = equ2ecl.applyTo(posEquatorial, new Ecliptical.Cart());
 
             int offset = 3 * i;
             POS_J2000[offset] = pos.x;
@@ -55,20 +52,20 @@ final public class Stars
         }
     }
 
-    private Stars()
-    {
+    private Stars() {
     }
 
-    public static void computeEclipticalJ2000( Time time, double[] eclipticalPositions )
-    {
-        computeEclipticalJ2000( time.getTime(), eclipticalPositions, 0, 1 );
+    public static void computeEclipticalJ2000(Time time, double[] eclipticalPositions) {
+        computeEclipticalJ2000(time.getTime(), eclipticalPositions, 0, 1);
     }
 
-    public static void computeEclipticalJ2000( long timeInMillis, double[] eclipticalPositions, int start, int delta )
-    {
-        final double yearsSince2000 = Time.julianYearsSinceJ2000( timeInMillis );
-        for ( int i = start; i < StarsCatalog.SIZE; i += delta )
-        {
+    public static void computeEclipticalJ2000(long timeInMillis, double[] eclipticalPositions, int start, int delta) {
+        final double yearsSince2000 = Time.julianYearsSinceJ2000(timeInMillis);
+        computeEclipticalJ2000(yearsSince2000, eclipticalPositions, start, delta);
+    }
+
+    public static void computeEclipticalJ2000(double yearsSince2000, double[] eclipticalPositions, int start, int delta) {
+        for (int i = start; i < StarsCatalog.SIZE; i += delta) {
             final int offset = 3 * i;
             // Compute ecliptical cartesian coordinates for J2000 frame
             eclipticalPositions[offset] = POS_J2000[offset] + VEL_J2000[offset] * yearsSince2000;
@@ -77,8 +74,7 @@ final public class Stars
         }
     }
 
-    public static void computeEquatorial( int starIndex, Time time, Equatorial.Cart outputPosition )
-    {
+    public static void computeEquatorial(int starIndex, Time time, Equatorial.Cart outputPosition) {
         final double yearsSince2000 = time.julianYearsSinceJ2000();
         final Ecliptic ecliptic = new Ecliptic(time);
         final Matrix3x3 transformation = new Matrix3x3(ecliptic.getTrafoEclJ2000ToMeanEquToDate());
@@ -87,16 +83,14 @@ final public class Stars
         outputPosition.x = POS_J2000[offset] + VEL_J2000[offset] * yearsSince2000;
         outputPosition.y = POS_J2000[offset + 1] + VEL_J2000[offset + 1] * yearsSince2000;
         outputPosition.z = POS_J2000[offset + 2] + VEL_J2000[offset + 2] * yearsSince2000;
-        transformation.applyTo( outputPosition ).normalize();
+        transformation.applyTo(outputPosition).normalize();
     }
 
-    public static void computeConstellationCenter( Constellation constellation, double[] starsCoordinates, Cartesian center )
-    {
-        center.set( 0, 0, 0 );
-        for ( int star : constellation.getStarList() )
-        {
+    public static void computeConstellationCenter(Constellation constellation, double[] starsCoordinates, Cartesian center) {
+        center.set(0, 0, 0);
+        for (int star : constellation.getStarList()) {
             int index = star * 3;
-            center.add( starsCoordinates[index], starsCoordinates[++index], starsCoordinates[++index] );
+            center.add(starsCoordinates[index], starsCoordinates[++index], starsCoordinates[++index]);
         }
         center.normalize();
     }
--- src/test/java/com/mkreidl/ephemeris/sky/StarPositionTest.kt
+++ src/test/java/com/mkreidl/ephemeris/sky/StarPositionTest.kt
@@ -1,5 +1,8 @@
 package com.mkreidl.ephemeris.sky
 
+import com.mkreidl.ephemeris.Time
+import com.mkreidl.ephemeris.solarsystem.Ecliptic
+import com.mkreidl.math.Vector3
 import org.junit.Assert
 import org.junit.Test
 
@@ -20,5 +23,22 @@ class StarPositionTest {
         Assert.assertEquals(deJ2000, de, tol)
     }
 
-    
+    val epoch = 2_462_088.69  // Julian day number of November 13.19, 2028
+    val raEpoch = Math.toRadians((2 + 44.0 / 60 + 12.975 / 3600) * 15)
+    val deEpoch = Math.toRadians(49 + 13.0 / 60 + 39.90 / 3600)
+
+    @Test
+    fun testThetaPersei2028November13() {
+        val julianYears = (epoch - Time.J2000.julianDayNumber()) / Time.DAYS_PER_YEAR
+        val output = DoubleArray(3 * StarsCatalog.SIZE)
+
+        Stars.computeEclipticalJ2000(julianYears, output, 0, 1)
+
+        val cartesian = Vector3(output[3 * 588], output[3 * 588 + 1], output[3 * 588 + 2])
+
+        val spherical = (Ecliptic(Time.J2000).trafoEcl2MeanEqu * cartesian).toSpherical()
+
+        Assert.assertEquals(raEpoch, spherical.lon, tol)
+        Assert.assertEquals(deEpoch, spherical.lat, tol)
+    }
 }
